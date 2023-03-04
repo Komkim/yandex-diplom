a5a625b202bc36c7b6588db1f95b513d49f98cda
Komkim <komkim@mail.ru>
2023-03-05 01:31:45 +0300

    add goose migration

---
+++ storage/postgres/migrations/20230305003321_create_enum_status_order.go
@@ -0,0 +1,22 @@
+package migrations
+
+import (
+	"database/sql"
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigration(upCreateEnumStatusOrder, downCreateEnumStatusOrder)
+}
+
+func upCreateEnumStatusOrder(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+		CREATE TYPE status AS ENUM ('REGISTERED', 'INVALID', 'PROCESSING', 'PROCESSED');
+	`)
+	return err
+}
+
+func downCreateEnumStatusOrder(tx *sql.Tx) error {
+	_, err := tx.Exec("drop type status")
+	return err
+}
---
+++ storage/postgres/migrations/20230305004335_create_orders.go
@@ -0,0 +1,32 @@
+package migrations
+
+import (
+	"database/sql"
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigration(upCreateOrders, downCreateOrders)
+}
+
+func upCreateOrders(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+			create table orders
+			(
+				id        serial not null primary key,
+				user_id   serial REFERENCES user (id),
+				balance_id serial REFERENCES balance (id),				
+				number	  integer not null,
+				status status not null,
+				accrual double precision,
+				withdraw double precision,
+				create_at timestamp with time zone default current_timestamp
+			);
+	`)
+	return err
+}
+
+func downCreateOrders(tx *sql.Tx) error {
+	_, err := tx.Exec("drop table orders")
+	return err
+}
